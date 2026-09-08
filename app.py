3cb154ad01dadf560b644b75a679b869b669dcf3
Pankaj Kumar <pankjku5698@gmail.com>
2026-09-08 12:47:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -958,9 +958,7 @@ class ResumeApp:
                             st.download_button(
                                 label="Download Resume 📥",
                                 data=resume_buffer,
-                                file_name=f"{
-    current_name.replace(
-        ' ', '_')}_resume.docx",
+                                file_name=f"{current_name.replace(' ', '_')}_resume.docx",
                                 mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document",
                                 on_click=lambda: st.balloons()
                             )
